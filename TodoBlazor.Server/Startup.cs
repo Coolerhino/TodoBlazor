6aae309ce509416d790b1f734725a7f809fe079a
Emilian Lato <emilian.lato@billennium.com>
2019-10-17 15:59:11 +0200

    Implement interaction with database

--- TodoBlazor.Server/Startup.cs
+++ TodoBlazor.Server/Startup.cs
@@ -2,12 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Net.Http.Headers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using TodoBlazor.Data;
 
 namespace TodoBlazor.Server
@@ -21,6 +23,14 @@ namespace TodoBlazor.Server
             services.AddDbContext<TodoContext>(
                 options => options.UseSqlServer(
                     "Server=(localdb)\\mssqllocaldb;Database=TodoDb;Trusted_Connection=True;MultipleActiveResultSets=true"));
+            
+            services.AddCors();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
+            services.AddMvcCore().AddApiExplorer();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -31,17 +41,27 @@ namespace TodoBlazor.Server
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My Api V1"));
+
+            app.UseCors(policy =>
+                policy.WithOrigins("http://localhost:49817")
+                .AllowAnyMethod()
+                .WithHeaders(HeaderNames.ContentType, HeaderNames.Authorization)
+                .AllowCredentials());
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllers();
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Hello World!");
                 });
             });
-
-            
         }
     }
 }
